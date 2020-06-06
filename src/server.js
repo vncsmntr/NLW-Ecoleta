7ed9950c1f44a823824c2699d35dcc64620bd296
Vinicius Monteiro <vncsmntr.work@gmail.com>
2020-06-06 13:35:16 -0300

    Configuring nonjucks

--- src/server.js
+++ src/server.js
@@ -1,4 +1,30 @@
-const express = require("express");
-const server = express();
+const express = require("express")
+const server = express()
 
-server.listen(3000);
+server.use(express.static("public"))
+
+
+// NunJucks Config - Templete Engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/view", {
+  express: server,
+  noCache: true
+})
+
+// --------- //
+//Home
+server.get("/", (req, res) => {
+  return res.render("index.html", { title: "Seu marketplace de coleta de resíduos" })
+})
+//Create Point
+server.get("/create-point", (req, res) => {
+  return res.render("create-point.html")
+})
+//Search Results
+server.get("/search", (req, res) => {
+  return res.render("search-results.html")
+})
+// --------- //
+
+
+server.listen(3000)
